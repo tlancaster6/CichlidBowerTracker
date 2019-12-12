8c9e7e5dfcb692447d794d45d7a612cbe9a45aa4
tlancaster6 <tuckerlancaster@gmail.com>
2019-12-12 14:11:46 -0500

    creating an outfile parser

---
+++ Modules/DataObjects/OutfileParser.py
@@ -0,0 +1,43 @@
+import os, glob, re
+import pandas as pd
+
+"""Reads the output/error files produced by a PACE run and parses the information into a dataframe"""
+
+
+class OutfileParser:
+
+    def __init__(self, projFileManager):
+        self.projFileManager = projFileManager
+
+    def validateInputData(self):
+        assert len(glob.glob(self.projFileManager.localTroubleshootingDir + '*.out*')) > 0,\
+            'Error: No Outfiles in Local Troubleshooting Directory'
+
+    def parseOutfiles(self):
+        regexes = {'video': re.compile(r'Processing video: Videos/(?P<video>.*),'),
+                   'requested': re.compile(r'Resources:(?P<requested>.*)\n'),
+                   'used': re.compile(r'Rsrc Used:(?P<used>.*)\n'),
+                   'start': re.compile(r'Begin PBS Prologue (?P<start>.*)\n'),
+                   'end': re.compile(r'End PBS Epilogue (?P<end>.*)\n'),
+                   'outcome': re.compile(r'PBS: job killed: (?P<outcome>.*)\n')}
+
+        rows = []
+        for f_name in glob.glob(self.projFileManager.localTroubleshootingDir + '*.out*'):
+            row = {}
+            with open(f_name) as f:
+                line = f.readline()
+                while line:
+                    for key, rx in regexes.items():
+                        match = rx.search(line)
+                        if match:
+                            if (key == 'requested') or (key == 'used'):
+                                row.update([tuple(item.split('=', 1)) for item in re.split(r',|\D:\D', match.group(key))])
+                            else:
+                                row.update({key: match.group(key)})
+                            if 'outcome' not in row.keys():
+                                row.update({'outcome': 'successful'})
+                    line = f.readline()
+            rows.append(row)
+        all_data = pd.DataFrame(rows)
+        all_data.to_csv(self.projFileManager.localTroubleshootingDir + 'outfileSummary.csv')
+
--- Tester.py
+++ Tester.py
@@ -1,9 +1,8 @@
 from Modules.DataPreparers.FigurePreparer import FigurePreparer as FP
 from Modules.DataPreparers.ProjectPreparer import ProjectPreparer as PP
+from Modules.DataObjects.OutfileParser import OutfileParser as OP
 
-pp = PP('MC6_5')
-fp = FP(pp.projFileManager)
-ca = fp.ca_obj
-fp._createCombinedFigures()
-
+pp = PP('TI2_5_newtray')
+op = OP(pp.projFileManager)
+op.parseOutfiles()
 
