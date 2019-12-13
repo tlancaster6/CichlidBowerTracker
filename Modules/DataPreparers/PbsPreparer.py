45c32455fd0dcd82e58c87a6b5a08dd9884aac66
tlancaster6 <tuckerlancaster@gmail.com>
2019-12-13 10:34:53 -0500

    continuing pace porting

--- Modules/DataPreparers/PbsPreparer.py
+++ Modules/DataPreparers/PbsPreparer.py
@@ -17,7 +17,18 @@ class PbsPreparer:
         self.uploads = [(self.projFileManager.localPbsDir, self.projFileManager.cloudPbsDir, '0')]
 
     def createPBS(self):
+        # Generate Depth Analysis PBS
+        d = {'PROJECT_ID': self.projFileManager.projectID, 'EMAIL': self.email}
+        infile = open(os.path.join(os.getcwd(), 'Modules', 'PbsTemplates', 'DepthAnalysis.pbs'), 'r')
+        mod = Template(infile.read()).safe_substitute(d)
+        infile.close()
+        if self.email is None:
+            mod = mod.replace('#PBS -M None\n#PBS -m abe\n', '')
+        outfile = open(self.projFileManager.localPbsDir + 'DepthAnalysis.pbs', 'w')
+        outfile.write(mod)
+        outfile.close()
 
+        # generate Cluster Analysis PBS
         array_max = str(LP(self.projFileManager.localLogfile).numDays - 1)
         d = {'ARRAY_MAX': array_max,
              'PROJECT_ID': self.projFileManager.projectID,
@@ -34,7 +45,7 @@ class PbsPreparer:
         outfile.close()
 
         # Generate Post-Cluster-Analysis PBS
-        d = {'PROJECT_ID': self.projFileManager.projectID}
+        d = {'PROJECT_ID': self.projFileManager.projectID, 'EMAIL': self.email}
         infile = open(os.path.join(os.getcwd(), 'Modules', 'PbsTemplates', 'PostClusterAnalysis.pbs'), 'r')
         mod = Template(infile.read()).safe_substitute(d)
         infile.close()
--- Tester.py
+++ Tester.py
@@ -3,4 +3,4 @@ from Modules.DataPreparers.ProjectPreparer import ProjectPreparer as PP
 from Modules.DataObjects.OutfileParser import OutfileParser as OP
 
 pp = PP('TI2_5_newtray', 28, 'LSS')
-pp.runPacePrep()
+pp.runPacePrep(email='tlancaster6@gatech.edu')
