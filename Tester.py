e443630db04ddca1881e6e9e9a2eea0fad590d50
tlancaster6 <tuckerlancaster@gmail.com>
2019-12-18 09:05:04 -0500

    checking on outfiles from cluster analysis

--- Tester.py
+++ Tester.py
@@ -3,9 +3,6 @@ from Modules.DataPreparers.ProjectPreparer import ProjectPreparer as PP
 from Modules.DataObjects.OutfileParser import OutfileParser as OP
 
 pp = PP('TI2_5_newtray', 64, 'LSS')
-pp.runPacePrep('tlancaster6@gatech.edu')
-
-
-
-# op = OP(pp.projFileManager)
-# op.parseOutfiles()
+# pp.runPacePrep('tlancaster6@gatech.edu')
+op = OP(pp.projFileManager)
+op.parseOutfiles()
