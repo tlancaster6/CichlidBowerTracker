39d56d921015d3173b5b1f318dfc8b52d5ca835d
Tucker L <tuckerlancaster@gmail.com>
2019-12-21 16:08:43 -0500

    still trying to track the video frag bug

--- Tester.py
+++ Tester.py
@@ -1,7 +1,7 @@
 import subprocess, os, sys
 
-h264_video = '0001_vid.h264'
-mp4_video = '0001_vid_test.mp4'
+h264_video = os.getenv('HOME') + '/scratch/TI2_5_newtray/Videos/0001_vid.h264'
+mp4_video = os.getenv('HOME') + '/scratch/TI2_5_newtray/Videos/0001_vid_test.mp4'
 command = ['ffmpeg', '-r', '30', '-i', h264_video, '-c:v', 'copy', '-r', '30', mp4_video, '-y']
 subprocess.run(command)
 assert os.path.isfile(mp4_video)
