a3034c05bb2b32dde1cca36b147e1cd6353e2599
Tucker L <tuckerlancaster@gmail.com>
2019-12-21 15:59:46 -0500

    still trying to track the video frag bug

--- Tester.py
+++ Tester.py
@@ -1,6 +1,12 @@
-import subprocess
+import subprocess, os, sys
 
 h264_video = '0001_vid.h264'
 mp4_video = '0001_vid_test.mp4'
 command = ['ffmpeg', '-r', '30', '-i', h264_video, '-c:v', 'copy', '-r', '30', mp4_video, '-y']
 subprocess.run(command)
+assert os.path.isfile(mp4_video)
+try:
+    assert os.stat(mp4_video).st_size >= os.stat(h264_video).st_size
+except AssertionError:
+    print('Bad Conversion')
+    sys.exit()
