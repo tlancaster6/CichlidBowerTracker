0be29dc495f2eeabf34bfd6bd2dd2392c91f7133
Tucker L <tuckerlancaster@gmail.com>
2019-12-21 14:54:43 -0500

    still trying to track the video frag bug

--- Modules/DataPreparers/VideoPreparer.py
+++ Modules/DataPreparers/VideoPreparer.py
@@ -77,7 +77,7 @@ class VideoPreparer:
 	def _convertVideo(self, mp4_video):
 		h264_video = mp4_video.replace('.mp4', '.h264')
 		assert os.path.isfile(h264_video)
-		command = ['ffmpeg', '-r', str(self.videoObj.framerate), '-i', h264_video, '-c:v', 'copy', '-r', str(self.videoObj.framerate), mp4_video, '-y', '-nostdin']
+		command = ['ffmpeg', '-r', str(self.videoObj.framerate), '-i', h264_video, '-c:v', 'copy', '-r', str(self.videoObj.framerate), mp4_video, '-y', '</dev/null']
 		print('  VideoConversion: ' + ' '.join(command) + ',Time' + str(datetime.datetime.now()))
 		f = open('remoteRemuxing.out', 'w')
 		output = subprocess.run(command, stdout=f, stderr=f)
