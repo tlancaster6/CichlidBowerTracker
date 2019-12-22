9b414ee2918c40ccd4818ba2ba59a8e0170a357e
Tucker L <tuckerlancaster@gmail.com>
2019-12-22 10:13:24 -0500

    still trying to track the video frag bug

--- Tester.py
+++ Tester.py
@@ -8,7 +8,7 @@ class Tester:
 
     def _convertVideo(self):
         h264_video = self.mp4_video.replace('.mp4', '.h264')
-        assert os.path.isfile(h264_video)
+        # assert os.path.isfile(h264_video)
         command = ['ffmpeg', '-r', '30', '-i', h264_video, '-c:v', 'copy', '-r', '30', self.mp4_video, '-y']
         print('  VideoConversion: ' + ' '.join(command) + ',Time' + str(datetime.datetime.now()))
         output = subprocess.run(command)
@@ -16,11 +16,11 @@ class Tester:
         assert os.path.isfile(self.mp4_video)
 
         # Ensure the conversion went ok.
-        try:
-            assert os.stat(self.mp4_video).st_size >= os.stat(h264_video).st_size
-        except AssertionError:
-            print('Bad Conversion')
-            sys.exit()
+        # try:
+        #     assert os.stat(self.mp4_video).st_size >= os.stat(h264_video).st_size
+        # except AssertionError:
+        #     print('Bad Conversion')
+        #     sys.exit()
 
 
 Tester()._convertVideo()
