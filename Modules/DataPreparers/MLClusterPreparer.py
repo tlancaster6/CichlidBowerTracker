bf2befb97e68465c157ee19b7c9e372deaed8951
tlancaster6 <tuckerlancaster@gmail.com>
2020-01-09 09:46:17 -0500

    debugging ML cluster prep

--- Modules/DataPreparers/MLClusterPreparer.py
+++ Modules/DataPreparers/MLClusterPreparer.py
@@ -41,68 +41,73 @@ class MLClusterPreparer:
 				self.videoClasses.append(tokens[1])
 
 	def _prepareClips(self):
-		print('Converting clips into jpgs and calculating means,,Time: ' + str(datetime.datetime.now())) 
+		print('untarring prepped clips')
+		subprocess.run(['tar', '-xvf', self.projFileManager.localMasterDir + 'preppedClips.tar', '-C',
+						self.projFileManager.localTempDir])
+		print('untarring complete. Temp directory contents:')
+		print(os.listdir(self.projFileManager.localTempDir))
 
-		clips = [x for x in os.listdir(self.projFileManager.localAllClipsDir) if '.mp4' in x]
-		assert len(clips) != 0
-
-		with open(self.projFileManager.localMasterDir + 'MeansAll.csv', 'w') as f, open(self.projFileManager.localMasterDir + 'cichlids_test_list.txt', 'w') as g:
-			print('Clip,MeanR,MeanG,MeanB,StdR,StdG,StdB', file = f)
-			for clip in clips:
-				label = self.videoClasses[0] # Need to temporarily assign the clip to a label - just pick the first
-					
-				outDirectory = self.projFileManager.localTempDir + label + '/' + clip.replace('.mp4','') + '/'
-
-				shutil.rmtree(outDirectory) if os.path.exists(outDirectory) else None
-				os.makedirs(outDirectory) 
-
-				outdata = subprocess.run(['ffmpeg', '-i', self.projFileManager.localAllClipsDir + clip, outDirectory + 'image_%05d.jpg', '-nostdin', '-loglevel', 'error'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-				print(outdata.stdout)
-				print(outdata.stderr)
-				#print(['ffmpeg', '-i', self.projFileManager.localAllClipsDir + clip, outDirectory + 'image_%05d.jpg'])
-
-				frames = [x for x in os.listdir(outDirectory) if '.jpg' in x]
-				try:
-					if self.nFrames != len(frames):
-						print('Different number of frames than expected in {0}. Expected "{1}, found {2}'.format(clip, self.nFrames, len(frames)))
-				except AttributeError:
-					self.nFrames = len(frames)
-
-				with open(outDirectory + 'n_frames', 'w') as i:
-					print(str(self.nFrames), file = i)
-
-				if len(frames) != 0:
-					img = io.imread(outDirectory + frames[0])
-					mean = img.mean(axis = (0,1))
-					std = img.std(axis = (0,1))
-					print(clip.replace('.mp4', '') + ',' + ','.join([str(x) for x in mean]) + ',' + ','.join([str(x) for x in std]), file = f)
-					print(label + '/' + clip.replace('.mp4',''), file = g)
-
-		subprocess.run(['touch', self.projFileManager.localMasterDir + 'cichlids_train_list.txt'])
-
-
-		dt = pd.read_csv(self.projFileManager.localMasterDir + 'MeansAll.csv', sep = ',')
-		dt['MeanID'] = dt.apply(lambda row: row.Clip.split('__')[0], axis = 1)
-		means = dt.groupby('MeanID').mean()
-
-		with open(self.projFileManager.localMasterDir + 'Means.csv', 'w') as f:
-			print('meanID,redMean,greenMean,blueMean,redStd,greenStd,blueStd', file = f)
-			for row in means.itertuples():
-				print(row.Index + ',' + str(row.MeanR) + ',' + str(row.MeanG) + ',' + str(row.MeanB) + ',' + str(row.StdR) + ',' + str(row.StdG) + ',' + str(row.StdB), file = f)
-
-		with open(self.projFileManager.localMasterDir + 'AnnotationFile.csv', 'w') as f:
-			print('Location,Dataset,Label,MeanID', file = f)
-			for row in dt.itertuples():
-				print(row.Clip + ',Test,' + label + ',' + row.MeanID, file = f)
-
-
-		command = []
-		command += ['python3', self.mlFileManager.localVideoPythonJsonFile]
-		command += [self.mlFileManager.localVideoClassesFile]
-		command += [self.projFileManager.localMasterDir + 'cichlids_train_list.txt']
-		command += [self.projFileManager.localMasterDir + 'cichlids_test_list.txt']
-		command += [self.projFileManager.localMasterDir + 'cichlids.json']
-		subprocess.call(command)
+	# print('Converting clips into jpgs and calculating means,,Time: ' + str(datetime.datetime.now()))
+		#
+		# clips = [x for x in os.listdir(self.projFileManager.localAllClipsDir) if '.mp4' in x]
+		# assert len(clips) != 0
+		#
+		# with open(self.projFileManager.localMasterDir + 'MeansAll.csv', 'w') as f, open(self.projFileManager.localMasterDir + 'cichlids_test_list.txt', 'w') as g:
+		# 	print('Clip,MeanR,MeanG,MeanB,StdR,StdG,StdB', file = f)
+		# 	for clip in clips:
+		# 		label = self.videoClasses[0] # Need to temporarily assign the clip to a label - just pick the first
+		#
+		# 		outDirectory = self.projFileManager.localTempDir + label + '/' + clip.replace('.mp4','') + '/'
+		#
+		# 		shutil.rmtree(outDirectory) if os.path.exists(outDirectory) else None
+		# 		os.makedirs(outDirectory)
+		#
+		# 		outdata = subprocess.run(['ffmpeg', '-i', self.projFileManager.localAllClipsDir + clip, outDirectory + 'image_%05d.jpg', '-nostdin', '-loglevel', 'error'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+		# 		print(outdata.stdout)
+		# 		#print(['ffmpeg', '-i', self.projFileManager.localAllClipsDir + clip, outDirectory + 'image_%05d.jpg'])
+		#
+		# 		frames = [x for x in os.listdir(outDirectory) if '.jpg' in x]
+		# 		try:
+		# 			if self.nFrames != len(frames):
+		# 				print('Different number of frames than expected in {0}. Expected "{1}, found {2}'.format(clip, self.nFrames, len(frames)))
+		# 		except AttributeError:
+		# 			self.nFrames = len(frames)
+		#
+		# 		with open(outDirectory + 'n_frames', 'w') as i:
+		# 			print(str(self.nFrames), file = i)
+		#
+		# 		if len(frames) != 0:
+		# 			img = io.imread(outDirectory + frames[0])
+		# 			mean = img.mean(axis = (0,1))
+		# 			std = img.std(axis = (0,1))
+		# 			print(clip.replace('.mp4', '') + ',' + ','.join([str(x) for x in mean]) + ',' + ','.join([str(x) for x in std]), file = f)
+		# 			print(label + '/' + clip.replace('.mp4',''), file = g)
+		#
+		# subprocess.run(['touch', self.projFileManager.localMasterDir + 'cichlids_train_list.txt'])
+		#
+		#
+		# dt = pd.read_csv(self.projFileManager.localMasterDir + 'MeansAll.csv', sep = ',')
+		# dt['MeanID'] = dt.apply(lambda row: row.Clip.split('__')[0], axis = 1)
+		# means = dt.groupby('MeanID').mean()
+		#
+		# with open(self.projFileManager.localMasterDir + 'Means.csv', 'w') as f:
+		# 	print('meanID,redMean,greenMean,blueMean,redStd,greenStd,blueStd', file = f)
+		# 	for row in means.itertuples():
+		# 		print(row.Index + ',' + str(row.MeanR) + ',' + str(row.MeanG) + ',' + str(row.MeanB) + ',' + str(row.StdR) + ',' + str(row.StdG) + ',' + str(row.StdB), file = f)
+		#
+		# with open(self.projFileManager.localMasterDir + 'AnnotationFile.csv', 'w') as f:
+		# 	print('Location,Dataset,Label,MeanID', file = f)
+		# 	for row in dt.itertuples():
+		# 		print(row.Clip + ',Test,' + label + ',' + row.MeanID, file = f)
+		#
+		#
+		# command = []
+		# command += ['python3', self.mlFileManager.localVideoPythonJsonFile]
+		# command += [self.mlFileManager.localVideoClassesFile]
+		# command += [self.projFileManager.localMasterDir + 'cichlids_train_list.txt']
+		# command += [self.projFileManager.localMasterDir + 'cichlids_test_list.txt']
+		# command += [self.projFileManager.localMasterDir + 'cichlids.json']
+		# subprocess.call(command)
 
 	def _predictLabels(self):
 
@@ -136,17 +141,9 @@ class MLClusterPreparer:
 		[outCommand.extend([str(a),str(b)]) for a,b in zip(command.keys(), command.values())]
 		outCommand = outCommand + ['--no_train']
 		print(' '.join(outCommand))
-		outdata = subprocess.run(outCommand, env=trainEnv, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		print(outdata.stdout)
-		print(outdata.stderr)
+		subprocess.run(outCommand, env=trainEnv)
 
-		try:
-			dt = pd.read_csv(self.projFileManager.localMasterDir + 'prediction/ConfidenceMatrix.csv', header = None, names = ['Filename'] + self.videoClasses, skiprows = [0], index_col = 0)
-		except FileNotFoundError:
-			print('Confidence Matrix file not found. backing up prepped clips as a tarfile')
-			subprocess.run(['tar', '-cvf', 'preppedClips.tar', '-C', self.projFileManager.localMasterDir, self.projFileManager.localTempDir])
-			raise FileNotFoundError
-
+		dt = pd.read_csv(self.projFileManager.localMasterDir + 'prediction/ConfidenceMatrix.csv', header = None, names = ['Filename'] + self.videoClasses, skiprows = [0], index_col = 0)
 		softmax = dt.apply(scipy.special.softmax, axis = 1)
 		prediction = pd.concat([softmax.idxmax(axis=1).rename(self.mlFileManager.vModelID + '_pred'), softmax.max(axis=1).rename(self.mlFileManager.vModelID + '_conf')], axis=1)
 		prediction['ClipName'] = prediction.apply(lambda row: row.name.split('/')[-1], axis = 1)
