0f87dc673b4d6645167bac2044ca4e7a3a9924ed
tlancaster6 <tuckerlancaster@gmail.com>
2020-01-16 15:24:20 -0500

    removing unnecessary prints

--- Modules/DataPreparers/MLClusterPreparer.py
+++ Modules/DataPreparers/MLClusterPreparer.py
@@ -133,10 +133,8 @@ class MLClusterPreparer:
 		outCommand = []
 		[outCommand.extend([str(a),str(b)]) for a,b in zip(command.keys(), command.values())]
 		outCommand = outCommand + ['--no_train']
-		print(' '.join(outCommand))
 		process = subprocess.run(outCommand, env=trainEnv, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
 		print(process.stderr)
-		print(process.stdout)
 
 		dt = pd.read_csv(self.projFileManager.localMasterDir + 'prediction/ConfidenceMatrix.csv', header = None, names = ['Filename'] + self.videoClasses, skiprows = [0], index_col = 0)
 		softmax = dt.apply(scipy.special.softmax, axis = 1)
