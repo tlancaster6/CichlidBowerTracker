261954324381e25995f18d921f95d63598a674a4
tlancaster6 <tuckerlancaster@gmail.com>
2020-01-24 10:31:38 -0500

    moving a print statement into the appropriate if clause

--- Modules/FileManagers/ProjFileManager.py
+++ Modules/FileManagers/ProjFileManager.py
@@ -245,8 +245,8 @@ class ProjFileManager:
 		# First try to download tarred Directory
 		tar_directory = directory[:-1] + '.tar'
 		output = subprocess.run(['rclone', 'copy', self.cloudMasterDir + tar_directory, self.localMasterDir, '--fast-list', '-v', '--checkers', '40', '--transfers', '40', '--tpslimit', '10'],stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8')
-		print(output.stdout)
 		if os.path.exists(self.localMasterDir + tar_directory):
+			print(output.stdout)
 			output = subprocess.run(['tar', '-xf', self.localMasterDir + tar_directory, '-C', self.localMasterDir], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8')
 			print(output.stdout)
 			if not os.path.exists(self.localMasterDir + directory):
